062c510df6aa2eddb49d73ef7c6416c8968a4cf3
fiorsaoirse <solovyanovamaria@yandex.ru>
2021-08-23 23:05:30 +0300

    +

--- promise.ts
+++ promise.ts
@@ -11,11 +11,11 @@ let counter = 1;
 
 type NonNull<T> = T extends null ? never : T;
 
-type FulfilledHandler<TResult1> = (value?: TResult1) => TResult1 | undefined;
-type RejectHandler<TResult2> = (reason?: any) => TResult2 | undefined;
+type FulfilledHandler<T> = (value?: any) => T | undefined;
+type RejectHandler<T> = (reason?: any) => T | undefined;
 
-// type ResolveFunction<T> = (value?: T) => void;
-// type RejectFunction<T> = (reason?: any) => void;
+type ResolveFunction<T> = (value?: T) => void;
+type RejectFunction<T> = (reason?: any) => void;
 
 // Обработчик по умолчанию просто возвращает тот же самый результат
 const defaultHandler = <T>(x?: T): T | undefined => x;
@@ -28,7 +28,7 @@ class CustomPromise<T = any> {
     PromiseFulfillReactions: FulfilledHandler<T>[];
     PromiseRejectReactions: RejectHandler<T>[];
 
-    constructor(executor: (resolve: (value: T) => void, reject: (reason: any) => void) => void) {
+    constructor(executor: (resolve: ResolveFunction<T>, reject: RejectFunction<T>) => void) {
         this.id = counter++;
         console.log(`pending promise with id: ${this.id}`);
         /**
@@ -96,53 +96,53 @@ class CustomPromise<T = any> {
         this.PromiseRejectReactions.forEach((rejectHandler) => rejectHandler(this.PromiseResult as NonNull<T>));
     }
 
-    then(onFulfill: FulfilledHandler<T> = defaultHandler, onReject: RejectHandler<T> = defaultHandler): CustomPromise<T> {
-        // Результатом выполнения будет новый промис, связанный с текущим
+    /**
+     * Обработчик then принимает 2 функции, первая - для успешного выполнения промиса, возвращает результат типа TResult1
+     * вторая - для обработки отклонения промиса, возвращает результат TResult2
+     */
+    then<TResult1 = T, TResult2 = never>(onFulfill: FulfilledHandler<TResult1> = defaultHandler, onReject: RejectHandler<TResult2> = defaultHandler): CustomPromise<TResult1 | TResult2> {
+        // Результатом выполнения будет новый промис ТИПА ПОЛУЧЕННОГО РЕЗУЛЬТАТА, связанный с текущим
         // Связь выражается в том, что новый промис проверяет СОСТОЯНИЕ текущего промиса и берет из него РЕЗУЛЬТАТ
-        return new CustomPromise<T>(
-            (
-                nextResolve: (value?: T) => void,
-                nextReject: (reason?: any) => void
-            ) => {
+        return new CustomPromise<TResult1 | TResult2>((nextResolve: ResolveFunction<TResult1 | TResult2>) => {
 
-                // Если промис разрешается УСПЕШНО, этот обработчик получит РЕЗУЛЬТАТ, который надо передать в
-                // функцию-коллбэк onFulfill, поступившую извне
-                const onFullfillHandler = (result?: T): void => {
-                    const fulfilledResult = onFulfill(result);
-                    nextResolve(fulfilledResult);
-                }
+            // Если промис разрешается УСПЕШНО, этот обработчик получит РЕЗУЛЬТАТ, который надо передать в
+            // функцию-коллбэк onFulfill, поступившую извне
+            const onFullfillHandler = (result?: T): void => {
+                const fulfilledResult = onFulfill(result);
+                nextResolve(fulfilledResult);
+            }
 
-                // Если промис разрешается НЕУСПЕШНО, этот обработчик получит ПРИЧИНУ,
-                //  которую надо передать в функцию-коллбэк onReject, поступившую извне
-                const onRejectHander = (reason?: any): void => {
-                    // Полученная причина передается в функцию-коллбэк onReject
-                    const rejectedResult = onReject(reason);
-                    // Затем новый промис РЕЗОЛВИТСЯ с результатом ТЕКУШЕГО
-                    nextResolve(rejectedResult);
-                }
+            // Если промис разрешается НЕУСПЕШНО, этот обработчик получит ПРИЧИНУ,
+            //  которую надо передать в функцию-коллбэк onReject, поступившую извне
+            const onRejectHander = (reason: any): void => {
+                // Полученная причина передается в функцию-коллбэк onReject
+                const rejectedResult = onReject(reason);
+                // Затем новый промис РЕЗОЛВИТСЯ с результатом ТЕКУШЕГО
+                nextResolve(rejectedResult);
+            }
 
-                if (this.PromiseState === STATES.FULFILLED) {
-                    // если промис уже зарезолвен, тогда НОВЫЙ промис просто РЕЗОЛВИМ с текущим готовым результатом
-                    // можно вызвать onFullfillHandler с текущим результатом, но так просто понятней
-                    // и все это на следующий тик
-                    setTimeout(() => nextResolve(onFulfill(this.PromiseResult as NonNull<T>)), 0);
-                }
+            if (this.PromiseState === STATES.FULFILLED) {
+                // если промис уже зарезолвен, тогда НОВЫЙ промис просто РЕЗОЛВИМ с текущим готовым результатом
+                // можно вызвать onFullfillHandler с текущим результатом, но так просто понятней
+                // и все это на следующий тик
+                setTimeout(() => nextResolve(onFulfill(this.PromiseResult as NonNull<T>)), 0);
+            }
 
-                if (this.PromiseState === STATES.REJECTED) {
-                    // если промис уже зареджектен, тогда новый промис РЕЗОЛВИТСЯ с текущим результатом (причиной)
-                    // пропущенным через обработчик ошибки (onRejectedHandler). Причина считается ОБРАБОТАННОЙ
-                    // и все это на следующий тик
-                    setTimeout(() => nextResolve(onReject(this.PromiseResult as NonNull<T>)), 0);
-                }
+            if (this.PromiseState === STATES.REJECTED) {
+                // если промис уже зареджектен, тогда новый промис РЕЗОЛВИТСЯ с текущим результатом (причиной)
+                // пропущенным через обработчик ошибки (onRejectedHandler). Причина считается ОБРАБОТАННОЙ
+                // и все это на следующий тик
+                setTimeout(() => nextResolve(onReject(this.PromiseResult as NonNull<T>)), 0);
+            }
 
-                // в ТЕКУЩИЙ промис добавляем новый обработчик на успешное выполнение
-                this.PromiseFulfillReactions.push(onFullfillHandler);
-                // в ТЕКУЩИЙ промис добавляем новый обработчик на неуспешное выполнение
-                this.PromiseRejectReactions.push(onRejectHander);
+            // в ТЕКУЩИЙ промис добавляем новый обработчик на успешное выполнение
+            this.PromiseFulfillReactions.push(onFullfillHandler);
+            // в ТЕКУЩИЙ промис добавляем новый обработчик на неуспешное выполнение
+            this.PromiseRejectReactions.push(onRejectHander);
 
-                // Основная суть в том, что промис - конечный автомат, а значит,
-                // будет исполнена либо цепочка then от текущего промиса, либо цепочка catch от текущего промиса
-            });
+            // Основная суть в том, что промис - конечный автомат, а значит,
+            // будет исполнена либо цепочка then от текущего промиса, либо цепочка catch от текущего промиса
+        });
     }
 }
 

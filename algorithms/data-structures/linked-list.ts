65436e15b39aa92b0ccc94b7d1e43c154c383de7
fiorsaoirse <solovyanovamaria@yandex.ru>
2021-09-20 22:38:19 +0300

    linked list done

--- algorithms/data-structures/linked-list.ts
+++ algorithms/data-structures/linked-list.ts
@@ -23,11 +23,12 @@ class LinkedList<T = any> {
     private tail: Node<T> | null;
     private _size: number;
 
-    // TODO: создать сразу из items?
     constructor(items?: Iterable<T>) {
         this.head = null;
         this.tail = null;
         this._size = 0;
+
+        this.fromArray(items);
     }
 
     get size(): number {
@@ -62,9 +63,11 @@ class LinkedList<T = any> {
 
     /**
      * 
-     * @param value значение для узла, который помещается в конец списка
+     * @param value значение для узла, который добавляется в список
+     * @param index - позиция, на которую надо вставлять элемент.
+     * Если индекс не указан, элемент просто добавляется в конец списка
      */
-    append(value: T): LinkedList<T> {
+    append(value: T, index?: number): LinkedList<T> {
         const node = new Node(value);
 
         if (!this.head || !this.tail) {
@@ -75,12 +78,30 @@ class LinkedList<T = any> {
             return this;
         }
 
-        const currentTail = this.tail;
-        currentTail.next = node;
-        node.previous = currentTail;
+        if (index === 0) {
+            this.prepend(value);
+        } else if (!index || index >= this.size || index < 0) {
+            const currentTail = this.tail;
+            currentTail.next = node;
+            node.previous = currentTail;
 
-        this.tail = node;
-        this._size += 1;
+            this.tail = node;
+            this._size += 1;
+        } else {
+            let nodeAtIndex: Node<T> = this.head;
+
+            for (let innerIndex = 0; innerIndex < index; innerIndex += 1) {
+                nodeAtIndex = nodeAtIndex.next as NonNullable<Node<T>>;
+            }
+
+            node.next = nodeAtIndex;
+            node.previous = nodeAtIndex.previous;
+
+            node.previous!.next = node;
+            nodeAtIndex.previous = node;
+
+            this._size += 1;
+        }
 
         return this;
     }
@@ -88,20 +109,22 @@ class LinkedList<T = any> {
     /**
      * 
      * @param value значение, узлы с которым необходимо удалить
-     * // TODO: чекануть жаву
-     * возвращает значение последнего удаленного узла (как в Java, но зачем?..)
+     * // TODO: чекануть жаву на тему возврата значения последнего
+     *  удаленного узла (но зачем?.. это же бессмысленно)
      */
-    delete(value: T): T | null {
-        if (!this.head) {
-            return null;
+    delete(value: T): void {
+        if (!this.head || !this.tail) {
+            return;
         }
 
         let currentNode: Node<T> | null = this.head;
-        let lastDeletedNode: Node<T> | null = null;
 
         while (currentNode) {
             if (currentNode.value === value) {
-                lastDeletedNode = currentNode;
+                if (currentNode === this.head && currentNode === this.tail) {
+                    this.head = this.tail = null;
+                    this._size -= 1;
+                }
 
                 const nextNode = currentNode.next;
 
@@ -129,9 +152,13 @@ class LinkedList<T = any> {
             }
         }
 
-        return lastDeletedNode?.value ?? null;
+        return;
     }
 
+    /**
+     * Удаляет головной узел в списке
+     * @returns значение удаленного узла или null, если такого узла нет
+     */
     deleteHead(): T | null {
         if (!this.head) {
             return null;
@@ -150,13 +177,31 @@ class LinkedList<T = any> {
         return value;
     }
 
-    deleteTail(): LinkedList<T> {
-        throw new Error('Method not implemented');
+    /**
+     * Удаляет хвостовой узел в списке
+     * @returns значение удаленного узла или null, если такого узла нет
+     */
+    deleteTail(): T | null {
+        if (!this.tail) {
+            return null;
+        }
+
+        const value = this.tail.value;
+
+        if (this.head === this.tail) {
+            this.head = this.tail = null;
+            return value;
+        }
+
+        this.tail = this.tail.previous;
+        this.tail!.next = null;
+
+        return value;
     }
 
     /**
      * 
-     * @returns возвращает массив со значениями всех узлов по очереди
+     * @returns массив со значениями всех узлов по очереди
      */
     toArray(): ReadonlyArray<T> {
         const result: Array<T> = [];
@@ -170,16 +215,61 @@ class LinkedList<T = any> {
         return result;
     }
 
-    fromArray(array: ReadonlyArray<T>): LinkedList {
-        throw new Error('Method not implemented');
+    /**
+     * 
+     * @param array итерируемый набор значений типа Т, из которого должны быть добавлены узлы
+     * в текущий список
+     * @returns связанный список типа Т
+     */
+    fromArray(items?: Iterable<T>): LinkedList {
+        if (items) {
+            for (const value of items) {
+                this.append(value);
+            }
+        }
+
+        return this;
     }
 
+    /**
+     * 
+     * @returns строковое представление значений узлов в том порядке,
+     * в котором они расположены в связанном списке
+     */
     toString(): string {
         return this.toArray().join(LinkedList.Separator);
     }
 
-    reverse(): LinkedList<T> {
-        throw new Error('Method not implemented');
+    /**
+     * 
+     * Разворачивает список - головной элемент становится хвостовым, и наоборот.
+     * Элементы между головными меняют ссылки на next и previous узлы
+     */
+    reverse(): void {
+        if (!this.head || !this.tail || this.head === this.tail) {
+            return;
+        }
+
+        let currentNode = this.head.next;
+
+        while (currentNode?.next) {
+            const { next, previous } = currentNode;
+            currentNode.previous = next;
+            currentNode.next = previous;
+
+            currentNode = next;
+        }
+
+        const { next } = this.head;
+        const { previous } = this.tail;
+
+        [this.head, this.tail] = [this.tail, this.head];
+
+        this.head.previous = null;
+        this.head.next = previous;
+
+        this.tail.next = null;
+        this.tail.previous = next;
     }
 
     private validate(): void | never {
@@ -195,21 +285,33 @@ class LinkedList<T = any> {
 
 const appendTest = (): void => {
     const list = new LinkedList<number>();
-    list.append(1);
-    list.append(2);
-    list.append(3);
+    list.append(1).append(2).append(3);
 
     assert(list.toString() === '1,2,3');
     assert(list.size === 3);
+
+    list.append(4, 2);
+
+    assert(list.toString() === '1,2,4,3');
+
+    list.append(5, 0);
+
+    assert(list.toString() === '5,1,2,4,3');
+
+    list.append(6, 5);
+
+    assert(list.toString() === '5,1,2,4,3,6');
+
+    list.append(7, 4);
+
+    assert(list.toString() === '5,1,2,4,7,3,6');
 
     console.log('SUCCESS appendTest');
 }
 
 const prependTest = (): void => {
     const list = new LinkedList<number>();
-    list.prepend(1);
-    list.prepend(2);
-    list.prepend(3);
+    list.prepend(1).prepend(2).prepend(3);
 
     assert(list.toString() === '3,2,1');
     assert(list.size === 3);
@@ -217,36 +319,136 @@ const prependTest = (): void => {
     console.log('SUCCESS prependTest');
 }
 
-const commonTest = (): void => {
+const deleteTest = () => {
+    const list = new LinkedList<string>();
+    list.append('foo').append('bar').append('baz').append('foo').append('boo');
+
+    assert(list.toString() === 'foo,bar,baz,foo,boo');
+
+    list.delete('foo');
+
+    assert(list.toString() === 'bar,baz,boo');
+
+    const anotherList = new LinkedList<string>();
+
+    assert(anotherList.toString() === '');
+
+    anotherList.delete('baz');
+
+    assert(anotherList.toString() === '');
+
+    anotherList.append('baz');
+
+    assert(anotherList.toString() === 'baz');
+
+    anotherList.delete('baz');
+
+    assert(anotherList.toString() === '');
+
+    console.log('SUCCESS deleteTest');
+}
+
+const deleteHeadTest = () => {
     const list = new LinkedList<number>();
+
+    const head1 = list.deleteHead();
+
+    assert(head1 === null);
+
     list.append(1);
     list.append(2);
-    list.prepend(0);
 
-    console.log(list.size);
-    assert(list.size === 3);
+    const head2 = list.deleteHead();
 
-    list.append(3);
-    list.prepend(2);
+    assert(head2 === 1);
+
+    const head3 = list.deleteHead();
+
+    assert(head3 === 2);
+
+    const head4 = list.deleteHead();
+
+    assert(head4 === null);
+
+    console.log('SUCCESS deleteHeadTest');
+}
+
+const deleteTailTest = () => {
+    const list = new LinkedList<number>();
+
+    const tail1 = list.deleteTail();
+
+    assert(tail1 === null);
+
+    list.append(1);
     list.append(2);
 
-    console.log(list.size);
-    // assert(list.size === 6);
+    const tail2 = list.deleteTail();
 
-    assert(list.toString() === '2,0,1,2,3,2');
+    assert(tail2 === 2);
 
-    const v = list.delete(2);
+    const tail3 = list.deleteTail();
 
-    console.log(list.size);
-    assert(list.toString() === '0,1,3');
-    assert(list.size === 3);
-};
+    assert(tail3 === 1);
+
+    const tail4 = list.deleteTail();
+
+    assert(tail4 === null);
+
+    console.log('SUCCESS deleteTailTest');
+}
+
+const toArrayTest = () => {
+    const list = new LinkedList<number>();
+    list.append(1).append(2).append(3).prepend(4);
+
+    const arr = list.toArray();
+    const testArr = [4, 1, 2, 3];
+
+    testArr.forEach((item: number, index: number) => {
+        assert(item === arr[index]);
+    });
+
+    console.log('SUCCESS toArrayTest');
+}
+
+const fromArrayTest = () => {
+    const array = ['one', 'two', 'three', 'four'];
+    const list = new LinkedList<string>();
+    list.fromArray(array);
+
+    const fromList = list.toArray();
+    array.forEach((item: string, index: number) => {
+        assert(item === fromList[index]);
+    });
+
+    console.log('SUCCESS fromArrayTest');
+}
+
+const reverseTest = () => {
+    const list = new LinkedList<number>();
+    list.append(1).append(2).append(3).append(4).append(5);
+
+    assert(list.toString() === '1,2,3,4,5');
+    assert(list.size === 5);
+
+    list.reverse();
+
+    assert(list.toString() === '5,4,3,2,1');
+    assert(list.size === 5);
+
+    console.log('SUCCESS reverseTest');
+}
 
 const test = () => {
     appendTest();
     prependTest();
-
-    // commonTest();
+    deleteTest();
+    deleteHeadTest();
+    deleteTailTest();
+    toArrayTest();
+    fromArrayTest();
+    reverseTest();
 }
 
 test();

23529d7a09ea3e617e247bc46ea605409f06a0dc
fiorsaoirse <solovyanovamaria@yandex.ru>
2022-10-12 12:18:30 +0300

    binary tree update

--- data-structures/tree/binary-tree/binary-tree.test.ts
+++ data-structures/tree/binary-tree/binary-tree.test.ts
@@ -4,7 +4,7 @@ describe('Binary Tree', () => {
     let tree: BinaryTree<number>;
 
     beforeEach(() => {
-        tree = new BinaryTree();
+        tree = new BinaryTree(8);
     });
 
     test('Only left side tree', () => {
@@ -15,11 +15,10 @@ describe('Binary Tree', () => {
          * |  2
          * |1   
          */
-        tree.add(8);
-        tree.add(6);
-        tree.add(5);
-        tree.add(2);
-        tree.add(1);
+        tree.insert(6);
+        tree.insert(5);
+        tree.insert(2);
+        tree.insert(1);
 
         const traversal = tree.traversal();
 
@@ -34,12 +33,10 @@ describe('Binary Tree', () => {
          * |  2
          * |    3   
          */
-
-        tree.add(8);
-        tree.add(6);
-        tree.add(5);
-        tree.add(2);
-        tree.add(3);
+        tree.insert(6);
+        tree.insert(5);
+        tree.insert(2);
+        tree.insert(3);
 
         const traversal = tree.traversal();
 
@@ -55,18 +52,16 @@ describe('Binary Tree', () => {
          *                        20
          * |   
          */
-
-        tree.add(8);
-        tree.add(5);
-        tree.add(9);
-        tree.add(12);
-        tree.add(14);
-        tree.add(10);
-        tree.add(6);
-        tree.add(3);
-        tree.add(2);
-        tree.add(4);
-        tree.add(20);
+        tree.insert(5);
+        tree.insert(9);
+        tree.insert(12);
+        tree.insert(14);
+        tree.insert(10);
+        tree.insert(6);
+        tree.insert(3);
+        tree.insert(2);
+        tree.insert(4);
+        tree.insert(20);
 
         expect(tree.leftHeight()).toBe(3);
         expect(tree.rightHeight()).toBe(4);
@@ -76,4 +71,84 @@ describe('Binary Tree', () => {
 
         expect(traversal).toEqual([2, 3, 4, 5, 6, 8, 9, 10, 12, 14, 20]);
     });
+
+    test('Check if binary tree is valid', () => {
+        const validTree = new BinaryTree(
+            5,
+            new BinaryTree(
+                3,
+                new BinaryTree(2),
+                new BinaryTree(4)
+            ),
+            new BinaryTree(
+                7,
+                new BinaryTree(6)
+            )
+        )
+
+        const valid = BinaryTree.isValid(validTree, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
+        expect(valid).toBe(true);
+
+        const invalidTree = new BinaryTree(
+            5,
+            new BinaryTree(
+                4,
+                new BinaryTree(2),
+                new BinaryTree(3)
+            ),
+            new BinaryTree(
+                7,
+                new BinaryTree(6)
+            )
+        );
+
+        const invalid = BinaryTree.isValid(invalidTree, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
+        expect(invalid).toBe(false);
+
+        const singleNodeTree = new BinaryTree(2);
+        const singleNode = BinaryTree.isValid(singleNodeTree, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
+        expect(singleNode).toBe(true);
+
+        const nullableTree = BinaryTree.isValid(null, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
+        expect(nullableTree).toBe(true);
+    })
+
+    test('Delete node from tree', () => {
+        /**|
+         * |          8
+         * |      5       9
+         * |    3   6  10   12
+         * |  2   4            14
+         *                        20
+         * |   
+         */
+        tree.insert(5);
+        tree.insert(9);
+        tree.insert(12);
+        tree.insert(14);
+        tree.insert(10);
+        tree.insert(6);
+        tree.insert(3);
+        tree.insert(2);
+        tree.insert(4);
+        tree.insert(20);
+
+        expect(tree.leftHeight()).toBe(3);
+        expect(tree.rightHeight()).toBe(4);
+        expect(tree.height()).toBe(8);
+
+        expect(tree.traversal()).toEqual([2, 3, 4, 5, 6, 8, 9, 10, 12, 14, 20]);
+
+        tree = BinaryTree.delete(tree, 1)!;
+        expect(tree.traversal()).toEqual([2, 3, 4, 5, 6, 8, 9, 10, 12, 14, 20]);
+
+        tree = BinaryTree.delete(tree, 5)!;
+        expect(tree.traversal()).toEqual([2, 3, 4, 6, 8, 9, 10, 12, 14, 20]);
+    })
+
+    test('Delete single node from tree', () => {
+        expect(tree.traversal()).toEqual([8]);
+        const result = BinaryTree.delete(tree, 8);
+        expect(result).toBe(null);
+    })
 })
--- data-structures/tree/binary-tree/binary-tree.ts
+++ data-structures/tree/binary-tree/binary-tree.ts
@@ -9,20 +9,18 @@ interface IBinaryTree<T> {
     leftHeight: () => number;
     rightHeight: () => number;
 
-    add: (value: T) => void;
+    insert: (value: T) => void;
     traversal: () => T[];
-
-    isBalanced: () => boolean;
 }
 
 class BinaryTree<T = number> implements IBinaryTree<T> {
-    public value: T | null;
-    public left: IBinaryTree<T> | null;
-    public right: IBinaryTree<T> | null;
+    public value: T;
+    public left: BinaryTree<T> | null;
+    public right: BinaryTree<T> | null;
 
-    next: IBinaryTree<T> | null;
+    next: BinaryTree<T> | null;
 
-    constructor(value: T | null = null, left: BinaryTree<T> | null = null, right: BinaryTree<T> | null = null) {
+    constructor(value: T, left: BinaryTree<T> | null = null, right: BinaryTree<T> | null = null) {
         this.value = value;
         this.left = left;
         this.right = right;
@@ -30,24 +28,19 @@ class BinaryTree<T = number> implements IBinaryTree<T> {
         this.next = null;
     }
 
-    public add(value: T) {
-        if (!this.value) {
-            this.value = value;
-        }
-
+    public insert(value: T): void {
         if (this.value > value) {
             if (!this.left) {
-                this.left = new BinaryTree();
+                this.left = new BinaryTree(value);
+            } else {
+                this.left.insert(value);
             }
-
-            this.left.add(value);
-
-        } else if (this.value < value) {
+        } else {
             if (!this.right) {
-                this.right = new BinaryTree();
+                this.right = new BinaryTree(value);
+            } else {
+                this.right.insert(value);
             }
-
-            this.right.add(value);
         }
     }
 
@@ -57,20 +50,6 @@ class BinaryTree<T = number> implements IBinaryTree<T> {
         }
 
         return [...(this.left?.traversal() || []), this.value, ...(this.right?.traversal() || [])];
-
-        // const result: T[] = [];
-
-        // if (this.left) {
-        //     result.push(...this.left.traversal());
-        // }
-
-        // result.push(this.value);
-
-        // if (this.right) {
-        //     result.push(...this.right.traversal());
-        // }
-
-        // return result;
     }
 
     height(): number {
@@ -93,10 +72,82 @@ class BinaryTree<T = number> implements IBinaryTree<T> {
         return this.right.rightHeight() + 1;
     }
 
-    isBalanced(): boolean {
-        // TODO: implement
-        throw new Error('Method not implemented');
+    static isBalanced<T>(root: BinaryTree<T> | null): boolean {
+        return BinaryTree.dfsHeight(root) !== -1;
+    }
+
+    private static dfsHeight<T>(root: BinaryTree<T> | null): number {
+        if (!root) {
+            return 0;
+        }
+
+        const left = BinaryTree.dfsHeight(root.left);
+        if (left === -1) {
+            return -1;
+        }
+
+        const right = BinaryTree.dfsHeight(root.right);
+        if (right === -1) {
+            return -1;
+        }
+
+        if (Math.abs(left - right) > 1) {
+            return -1;
+        }
+
+        return Math.max(left, right) + 1;
+    }
+
+    static isValid<T>(root: BinaryTree<T> | null, min: T, max: T): boolean {
+        const inner = (tree: BinaryTree<T> | null, min: T, max: T): boolean => {
+            if (!tree) {
+                return true;
+            }
+
+            if (tree.value <= min || tree.value >= max) {
+                return false;
+            }
+
+            return inner(tree.left, min, tree.value) && inner(tree.right, tree.value, max);
+        }
+
+        return inner(root, min, max);
+    }
+
+    static delete<T>(root: BinaryTree<T> | null, value: T): BinaryTree<T> | null {
+        if (!root) {
+            return null;
+        }
+
+        if (root.value === value) {
+            if (!root.left && !root.right) {
+                return null;
+            }
+
+            if (!root.left || !root.right) {
+                return root.left ? root.left : root.right;
+            }
+
+            let temp = root.right;
+            while (temp.left) {
+                temp = temp.left;
+            }
+
+            root.value = temp.value;
+            root.right = BinaryTree.delete(root.right, temp.value);
+
+            return root;
+        }
+
+        if (root.value > value) {
+            BinaryTree.delete(root.left, value);
+        } else {
+            BinaryTree.delete(root.right, value);
+        }
+
+        return root;
     }
 }
 
 export { BinaryTree };
+
